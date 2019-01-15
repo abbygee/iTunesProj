7a0b395ef3d6f5a1b4d9d33e9dfc690e22b4378f
abbygee <abigaildelgado@students.berkeley.net>
2019-01-15 14:15:42 -0800

    final?

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     $("#submit").click(function(){
-        $.get("https://itunes.apple.com/search?term=" + $("#artist").val().toLocaleLowerCase(),processResults);
+        $.ajax({
+            url: "https://itunes.apple.com/search?term=" + $("#artist").val(),
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function (result) {
+                processResults(result);
+            },
+            error: function () {
+                alert('Failed!');
+            }
+        });
     });
 });
 
@@ -9,23 +20,20 @@ function processResults(data){
     //empties previous results
     $("#container").empty();
 
-    var result = JSON.parse(data);
-    console.log(JSON.parse(data));
-
     var container = $("#container");
     var table = '<table class="w3-table w3-striped w3-bordered w3-large" id="table">';
 
-    if(result.resultCount === 0){
+    if(data.resultCount === 0){
         container.append("Sorry there are no results :(");
     }
 
     for(var i = 0; i < parseInt($("#options").val()); i++){
-        var image = result.results[i].artworkUrl100;
-        var audio = result.results[i].previewUrl;
+        var image = data.results[i].artworkUrl100;
+        var audio = data.results[i].previewUrl;
 
         table += '<tr id="' + i + '">';
         table += '<td><img src=' + '"' + image + '"' + '></td>';
-        table += "<td><div>Ranked #" + (i+1) + "</div><div>" + result.results[i].trackName + "</div><div>" + result.results[i].artistName + "</div><div>" + result.results[i].collectionName + "</div></td>";
+        table += "<td><div>Ranked #" + (i+1) + "</div><div>" + data.results[i].trackName + "</div><div>" + data.results[i].artistName + "</div><div>" + data.results[i].collectionName + "</div></td>";
         table += '<td><audio controls class="audio"> <source src="' + audio + '"></audio></td>';
     }
 
